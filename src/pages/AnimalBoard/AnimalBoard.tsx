3239a9aece3c625e12d829a6c3f6a98af4691ce8
SeungHyeon-web <tmdgus1326@gmail.com>
2025-02-18 15:19:02 +0900

    ✨ feat: 라우터 설정정

---
+++ src/pages/AnimalBoard/AnimalBoard.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AnimalBoard() {
+  return <div>AnimalBoard</div>;
+}
---
+++ src/pages/EditCommunity/EditCommunity.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EditCommunity() {
+  return <div>EditCommunity</div>;
+}
