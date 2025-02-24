1802e825752812d03157354cbfdc02a704e4b9b2
yoongjeonging <yooong.o353@gmail.com>
2025-02-24 09:32:42 +0900

    :lipstick: design: donation-page publishing

--- src/pages/Donation/Donation.tsx
+++ src/pages/Donation/Donation.tsx
@@ -1,5 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../../assets/icons/logo.svg";
+import Input from "../../common/InputComponent";
+import Button from "../../common/ButtonComponent";
+import DonationModal from "./components/DonationModal";
 
 export default function Donation() {
-  return <div>Donationss</div>;
+  const [selected, setSelected] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const handleSelect = (value: string) => {
+    setSelected((prev) => (prev === value ? null : value));
+  };
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <div className="flex justify-center my-5">
+      <div className="flex flex-col gap-4 justify-center items-center w-full max-w-[476px] h-[593px] border border-[#D9D9D9] rounded-3xl px-5">
+        <div className="flex gap-1">
+          <img src={logo} alt="사이트 로고" className="w-6 h-6" />
+          <p className="font-semibold text-[18px]">PAWEVER</p>
+        </div>
+        <p className="font-semibold text-[16px] mb-5 text-center">
+          후원금은 전액 동물보호센터에 후원되고 있습니다.
+        </p>
+
+        <div className="w-full space-y-4">
+          <div>
+            <p className="text-[14px] text-[#5F656C]">
+              후원자 이름(공백시 익명) <span className="text-[#F7585C]">*</span>
+            </p>
+            <Input
+              placeholder="후원자 이름을 입력해주세요."
+              className="h-[40px] bg-[#F4F6F8] border-none w-full"
+            />
+          </div>
+
+          <div>
+            <p className="text-[14px] text-[#5F656C]">
+              후원 금액 <span className="text-[#F7585C]">*</span>
+            </p>
+            <Input
+              placeholder="후원할 금액을 입력해주세요."
+              className="h-[40px] bg-[#F4F6F8] border-none w-full"
+            />
+          </div>
+
+          <div>
+            <p className="text-[14px] text-[#5F656C]">
+              남기고 싶은 말 <span className="text-[#F7585C]">*</span>
+            </p>
+            <Input
+              placeholder="후원에 대한 메시지를 남겨주세요."
+              className="h-[40px] bg-[#F4F6F8] border-none w-full"
+            />
+          </div>
+        </div>
+
+        <div className="w-full">
+          <p className="text-[14px] text-[#5F656C]">
+            결제 방법 <span className="text-[#F7585C]">*</span>
+          </p>
+          <div className="flex w-[424px] gap-2">
+            <button
+              className={`px-4 py-2 rounded-lg ${
+                selected === "card"
+                  ? "bg-main text-white"
+                  : "bg-[#F4F6F8]  text-[#9da3ae]"
+              }`}
+              onClick={() => handleSelect("card")}
+            >
+              신용/체크카드
+            </button>
+            <button
+              className={`px-4 py-2 rounded-lg ${
+                selected === "cash"
+                  ? "bg-main text-white"
+                  : "bg-[#F4F6F8]  text-[#9da3ae]"
+              }`}
+              onClick={() => handleSelect("cash")}
+            >
+              실시간계좌이체
+            </button>
+          </div>
+        </div>
+
+        <Button
+          className="w-full h-[64px] mt-5 border-none"
+          onClick={openModal}
+        >
+          후원하기
+        </Button>
+        <DonationModal isModalOpen={isModalOpen} closeModal={closeModal} />
+      </div>
+    </div>
+  );
 }
---
+++ src/pages/Donation/components/DonationModal.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Lottie from "lottie-react";
+import paw from "../../../assets/images/paw.json";
+import logo from "../../../assets/icons/logo.svg";
+import Button from "../../../common/ButtonComponent";
+
+interface Modal {
+  isModalOpen: boolean;
+  closeModal: () => void;
+}
+export default function DonationModal({ isModalOpen, closeModal }: Modal) {
+  return (
+    isModalOpen && (
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 mt-10 px-4">
+        <div className="w-full max-w-sm sm:max-w-md h-[303px] bg-white rounded-3xl flex flex-col items-center justify-center px-4">
+          <div className="flex gap-1 ">
+            <img src={logo} alt="사이트로고" className="w-6 h-6" />
+            <p className="font-semibold text-[18px]">PAWEVER</p>
+          </div>
+          <p>후원해 주셔서 감사합니다</p>
+          <Lottie
+            animationData={paw}
+            loop={true}
+            className="w-[100px] h-[100px]"
+          />
+          <Button className="w-full h-12 " onClick={closeModal}>
+            확인
+          </Button>
+        </div>
+      </div>
+    )
+  );
+}
